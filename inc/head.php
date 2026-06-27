8f2250c7509ae7562915831edc031a60771f0dc6
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-27 11:28:18 +0100

    Update head.php

--- inc/head.php
+++ inc/head.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
@@ -87,4 +84,4 @@
   
  <link rel="stylesheet" href="assets/css/styles.css">
 
-</head><body>
+</head>
