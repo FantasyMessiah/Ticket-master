7708259d37820a20fe34408aff4c87ff378394ac
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-06-27 11:32:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    
 <?php include "inc/head.php"; ?>
 
-<div id="__next">  
-<?php include "inc/header.php"; ?>
-
+<body>
+<div id="__next">
     
+<?php include "inc/header.php"; ?>
 <?php include "inc/footer.php"; ?>
+
 </div>
+    
+<?php include "inc/script.php"; ?>
 
-<?php include "inc/script2.php"; ?>
-
-<div id="modals" data-testid="modals"></div></body></html>
+<div id="modals" data-testid="modals">
+</div>
+</body>
+</html>
